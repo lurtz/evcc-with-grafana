ceeaa8f831071bb9b7f8620fa02433e70d9ab635
Lutz <727209+lurtz@users.noreply.github.com>
2025-08-19 00:25:08 +0200

    Add smoke tests
    These tests ensure that least we may bring containers up and
    the credentials are passed correctly

---
+++ test/functions.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+function setup() {
+	# Restore old state exit
+	# TODO Running the tests in a container would be more robust
+	trap "make down; mv .env-back .env; mv evcc-provisioning/evcc.yaml-back evcc-provisioning/evcc.yaml" EXIT
+
+	# Save previous state
+	touch .env
+	touch evcc-provisioning/evcc.yaml
+	mv .env .env-back
+	mv evcc-provisioning/evcc.yaml evcc-provisioning/evcc.yaml-back
+}
+
+function write_fake_credentials() {
+	cat <<EOF > .env
+VICTORIAMETRICS_USER=victoria
+VICTORIAMETRICS_PASSWORD=metrics
+GRAFANA_USERNAME=graf
+GRAFANA_PASSWORD=ana
+EOF
+}
+
+function write_fake_evcc_config() {
+	cat <<EOF > evcc-provisioning/evcc.yaml
+network:
+  schema: http
+  host: evcc.local # .local suffix announces the hostname on MDNS
+  port: 7070
+
+log: debug
+levels:
+  cache: error
+
+# unique installation id
+plant: 66666662fe172da0b3c701997ee9d78dc8a27deff1f09634f1a7e24439700a28
+
+interval: 30s # control cycle interval
+
+influx:
+  url: http://victoria:metrics@victoria-metrics:8428
+
+meters:
+  - name: my_battery
+    type: template
+    template: demo-battery
+    usage: battery
+    power: 3000
+    soc: 0.5
+  - name: my_grid
+    type: template
+    template: demo-meter
+    usage: grid
+    power: 5000
+  - name: my_pv
+    type: template
+    template: demo-meter
+    usage: pv
+    power: 6000
+
+chargers:
+  - name: wallbox4
+    type: template
+    template: demo-charger
+    status: A # Ladezustand, [A, B, C]
+
+loadpoints:
+  - title: Garage1
+    charger: wallbox4
+    mode: minpv
+
+site:
+  title: Baumhaus
+  meters:
+    grid: my_grid
+    pv:
+      - my_pv
+    battery:
+      - my_battery
+EOF
+}
---
+++ test/smoke_test.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+set -euxo pipefail
+
+source "$(dirname "$0")/functions.sh"
+
+function startup_starts_all_containers_with_credentials_setup() {
+	setup
+	write_fake_credentials
+	write_fake_evcc_config
+
+	# bring it up
+	make up
+
+	# wait until all is running
+	while podman exec evcc pidof evcc; ss=$?; [ 1 -eq $ss ]; do
+		sleep 1
+	done
+
+	# check all daemons run
+	# check environment is as expected
+	[ 1 == $(podman exec evcc pidof evcc) ]
+	[ 1 == $(podman exec evcc-with-grafana_grafana_1 pidof grafana) ]
+	[ 1 == $(podman exec evcc-with-grafana_victoria-metrics_1 pidof /victoria-metrics-prod) ]
+	[ 1 == $(podman exec evcc-with-grafana_telegraf_1 pidof telegraf) ]
+
+	# check configuration is correct
+	local grafana_env="$(podman exec evcc-with-grafana_grafana_1 env)"
+	local telegraf_env="$(podman exec evcc-with-grafana_telegraf_1 env)"
+	local vm_command="$(podman exec evcc-with-grafana_victoria-metrics_1 cat /proc/1/cmdline)"
+
+	echo "$grafana_env" | grep -q "VM_USER=victoria"
+	echo "$grafana_env" | grep -q "VM_PASSWORD=metrics"
+	echo "$grafana_env" | grep -q "GF_SECURITY_ADMIN_PASSWORD=ana"
+	echo "$grafana_env" | grep -q "GF_SECURITY_ADMIN_USER=graf"
+	echo "$grafana_env" | grep -q "GF_AUTH_ANONYMOUS_ENABLED=true"
+	echo "$grafana_env" | grep -q "GF_AUTH_ANONYMOUS_ORG_ROLE=Viewer"
+
+	echo "$telegraf_env" | grep -q "VM_USER=victoria"
+	echo "$telegraf_env" | grep -q "VM_PASSWORD=metrics"
+
+	echo "$vm_command" | grep -q "Auth.usernamevictoria"
+	echo "$vm_command" | grep -q "Auth.passwordmetrics"
+}
+
+
+startup_starts_all_containers_with_credentials_setup
+
+echo -e "\nAll tests passed\n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
